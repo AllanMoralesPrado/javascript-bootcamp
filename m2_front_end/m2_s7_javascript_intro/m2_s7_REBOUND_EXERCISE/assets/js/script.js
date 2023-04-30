a5def209aacb17b966308105694fea9892d48043
Allan Morales Prado <allan.moralesp@utem.cl>
2023-04-29 20:36:21 -0400

    undo m3s1 checkpoint but no commit

--- m2_front_end/m2_s7_javascript_intro/m2_s7_REBOUND_EXERCISE/assets/js/script.js
+++
@@ -1,6 +0,0 @@
-let numero1 = parseInt(prompt("Ingrese primer número: "));
-let numero2 = parseInt(prompt("Ingrese segundo número: "));
-let proposicion = (numero1 > numero2) ? " es mayor que " : numero1 < numero2 ? " es menor que " : " es igual que ";
-
-
-alert(numero1 + `${proposicion}` + numero2);
