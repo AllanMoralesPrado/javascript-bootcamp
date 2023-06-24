89d687fdf5bd01a8de30a5ea32f84120f548dba7
Allan Morales Prado <allan.moralesp@utem.cl>
2023-06-23 20:50:31 -0400

    m6s4 TERMINADO

---
+++ m6_s4_DRILLING/datosPkmn/datos.js
@@ -0,0 +1,5 @@
+const pokemons = ["bulbasaur", "ivysaur", "venusaur", "squirtle", "wartortle", "blastoise", "charmander", "charmeleon", "charizard"];
+
+module.exports = {
+    pokemons:pokemons,
+}
